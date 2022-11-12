fb5e2165b2da5e733c92bcc9236ac6f3719a05c1
Yucel Moran <yucel.moran@gmail.com>
2022-11-11 20:04:01 -0500

    Final version for hackathon purposes

---
+++ force-app/main/default/staticresources/googleMapsPolygon.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	console.log(':: FIELDIRECT ::');
+	initApp();
+	$('#remove').hide();
+	$('.third').addClass('slds-hide');
+	$('#update').hide();
+	$('#pac-card').hide();
+});
